cbeb92d3e967fd4921423758b1be92d3f4fbbb09
Ghassanooooo <ghassanaldarwish@yahoo.com>
2023-11-15 13:24:24 +0100

    add react

--- vite-shadcn.sh
+++ vite-shadcn.sh
@@ -196,6 +196,7 @@ cat <<EOF > "$MAIN_PATH"
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
